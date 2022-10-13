5932e5292be73b83aaf1cbe945e6bc9d79733102
Antonio Correia <antonio.alves.correia@gmail.com>
2022-10-13 20:43:03 -0300

    creating project and architecturing with Blazor WASM + MAUI

---
+++ AgregaDotNet/AgregaDotNet/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AgregaDotNet;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AgregaDotNet/AgregaDotNet/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AgregaDotNet;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
