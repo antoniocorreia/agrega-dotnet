ab37b13e8ac628093693ce593c57f9173a698d52
Laura Ferraz <lauraferraz5@gmail.com>
2022-10-18 19:40:02 -0300

    Fixed styles

--- AgregaDotNet/AgregaDotNet.Razor.UI/wwwroot/js/app.js
+++ AgregaDotNet/AgregaDotNet.Razor.UI/wwwroot/js/app.js
@@ -19,10 +19,14 @@
  *     06.  Feather icon         *
  *     06.  DD Menu              *
  *     06.  Active Sidebar Menu  *
+ *     07.  Contact us           *
  ================================*/
 
 
- window.onload = function loader() {
+window.addEventListener('load',   fn , false )
+
+//  window.onload = function loader() {
+function fn() {
     // Preloader
     if(document.getElementById('preloader')){
         setTimeout(() => {
@@ -30,10 +34,8 @@
             document.getElementById('preloader').style.opacity = '0';
         }, 350);
     }
-
     // Menus
     activateMenu();
-    activateSidebarMenu();
 }
 
 //Menu
@@ -114,32 +116,6 @@ function activateMenu() {
     }
 }
 
-
-//Admin Menu
-function activateSidebarMenu() {
-    var current = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-    if (current !== "" && document.getElementById("sidebar")){
-        var menuItems = document.querySelectorAll('#sidebar a');
-        for (var i = 0, len = menuItems.length; i < len; i++) {
-            if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
-                menuItems[i].parentElement.className += " active";
-                if(menuItems[i].closest(".sidebar-submenu")) {
-                    menuItems[i].closest(".sidebar-submenu").classList.add("d-block");
-                }
-                if(menuItems[i].closest(".sidebar-dropdown")) {
-                    menuItems[i].closest(".sidebar-dropdown").classList.add("active");
-                }
-            }
-        }
-    }
-}
-
-if(document.getElementById("close-sidebar")){
-    document.getElementById("close-sidebar").addEventListener("click", function() {
-        document.getElementsByClassName("page-wrapper")[0].classList.toggle("toggled");
-    });
-}
-
 // Clickable Menu
 if(document.getElementById("navigation")){
     var elements = document.getElementById("navigation").getElementsByTagName("a");
@@ -153,32 +129,18 @@ if(document.getElementById("navigation")){
     }
 }
 
-if(document.getElementById("sidebar")){
-    var elements = document.getElementById("sidebar").getElementsByTagName("a");
-    for(var i = 0, len = elements.length; i < len; i++) {
-        elements[i].onclick = function (elem) {
-            if(elem.target !== document.querySelectorAll("li.sidebar-dropdown.active > a")[0]){
-                document.querySelectorAll("li.sidebar-dropdown.active")[0]?.classList?.toggle("active");
-                document.querySelectorAll("div.sidebar-submenu.d-block")[0]?.classList?.toggle("d-block");
-            }
-            if(elem.target.getAttribute("href") === "javascript:void(0)") {
-                elem.target.parentElement.classList.toggle("active");
-                elem.target.nextElementSibling.classList.toggle("d-block");
-            }
-        }
-    }
-}
-
 // Menu sticky
 function windowScroll() {
-    var navbar = document.getElementById("topnav");
-    if(navbar === null) {
-        
-    }else if( document.body.scrollTop >= 50 ||
-    document.documentElement.scrollTop >= 50){
-        navbar.classList.add("nav-sticky");
-    }else {
-        navbar.classList.remove("nav-sticky");
+    const navbar = document.getElementById("topnav");
+    if(navbar!=null){
+        if (
+            document.body.scrollTop >= 50 ||
+            document.documentElement.scrollTop >= 50
+        ) {
+            navbar.classList.add("nav-sticky");
+        } else {
+            navbar.classList.remove("nav-sticky");
+        }
     }
 }
 
@@ -188,37 +150,24 @@ window.addEventListener('scroll', (ev) => {
 })
 
 // back-to-top
+var mybutton = document.getElementById("back-to-top");
 window.onscroll = function () {
     scrollFunction();
 };
 
 function scrollFunction() {
-    var mybutton = document.getElementById("back-to-top");
-    if(mybutton === null) {
-        
-    }else if( document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
-        mybutton.style.display = "block";
-    }else {
-        mybutton.style.display = "none";
+    if(mybutton!=null){
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
     }
 }
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
-
-//Feather icon
-feather.replace();
-
-// dd-menu
-if(document.getElementsByClassName("dd-menu")) {
-    var ddmenu = document.getElementsByClassName("dd-menu");
-    for(var i = 0, len = ddmenu.length; i < len; i++) {
-        ddmenu[i].onclick = function (elem) {
-            elem.stopPropagation();
-        }
-    }
 }
 
 //ACtive Sidebar
@@ -233,29 +182,16 @@ if(document.getElementsByClassName("dd-menu")) {
     }
 })();
 
+//Feather icon
+feather.replace();
 
-//Validation Shop Checkouts
-(function () {
-    'use strict'
-
-    if(document.getElementsByClassName('needs-validation').length > 0) {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.querySelectorAll('.needs-validation')
-
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-            }, false)
-        })
+// dd-menu
+var ddmenu = document.getElementsByClassName("dd-menu");
+for(var i = 0, len = ddmenu.length; i < len; i++) {
+    ddmenu[i].onclick = function (elem) {
+        elem.stopPropagation();
     }
-})();
+}
 
 //Tooltip
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
@@ -268,3 +204,73 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+//small menu
+try {
+    var spy = new Gumshoe('#navmenu-nav a');
+}catch(err) {
+    
+}
+//********************* */
+/*     Contact us       */
+//********************* */
+try {
+    // Contact Form
+    function validateForm() {
+        var name = document.forms["myForm"]["name"].value;
+        var email = document.forms["myForm"]["email"].value;
+        var subject = document.forms["myForm"]["subject"].value;
+        var comments = document.forms["myForm"]["comments"].value;
+        document.getElementById("error-msg").style.opacity = 0;
+        document.getElementById('error-msg').innerHTML = "";
+        if (name == "" || name == null) {
+            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+            fadeIn();
+            return false;
+        }
+        if (email == "" || email == null) {
+            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
+            fadeIn();
+            return false;
+        }
+        if (subject == "" || subject == null) {
+            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+            fadeIn();
+            return false;
+        }
+        if (comments == "" || comments == null) {
+            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
+            fadeIn();
+            return false;
+        }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("simple-msg").innerHTML = this.responseText;
+                document.forms["myForm"]["name"].value = "";
+                document.forms["myForm"]["email"].value = "";
+                document.forms["myForm"]["subject"].value = "";
+                document.forms["myForm"]["comments"].value = "";
+            }
+        };
+        xhttp.open("POST", "php/contact.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+        return false;
+    }
+
+    function fadeIn() {
+        var fade = document.getElementById("error-msg");
+        var opacity = 0;
+        var intervalID = setInterval(function () {
+            if (opacity < 1) {
+                opacity = opacity + 0.5
+                fade.style.opacity = opacity;
+            } else {
+                clearInterval(intervalID);
+            }
+        }, 200);
+    }
+} catch (err) {
+
+}
