7f917d48a436797433b8c009e720fc66561ad670
Laura Ferraz <lauraferraz5@gmail.com>
2022-10-19 22:13:59 -0300

    .

---
+++ AgregaDotNet/AgregaDotNet.Web/Server/Posts/GetBlogs.cs
@@ -0,0 +1,26 @@
+﻿using AgregaDotNet.Features.Posts._Entities;
+using Microsoft.Extensions.Configuration;
+using Sparc.Features;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace AgregaDotNet.Features.Posts
+{
+    public class GetBlogs : PublicFeature<List<Blog>>
+    {
+        private IConfiguration _configuration;
+
+        public GetBlogs(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public override async Task<List<Blog>> ExecuteAsync()
+        {
+            var blogs = _configuration.GetSection("Blogs").Get<List<Blog>>();
+
+            return await Task.FromResult(blogs);
+        }
+
+    }
+}
